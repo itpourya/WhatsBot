eed4e615a92930ecc4b2fcb5fc07cb6f52382fd7
Pourya Samimi <neverdiegenius@gmail.com>
2024-07-03 18:52:43 +0330

    transfer project

---
+++ config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+USERNAME = "admin"
+PASSWORD = "admin"
+UPLOAD_FILE = "uploads/"
+DATABASE_PATH = "db/database.sqlite"
---
+++ main.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+
+from flask import Flask, flash
+from flask import Flask, Response, redirect, url_for, request, session, abort
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+import config
+import os
+from werkzeug.utils import secure_filename
+import import_number
+
+
+# upload file config
+UPLOAD_FOLDER = config.UPLOAD_FILE
+ALLOWED_EXTENSIONS = {'xlsx'}
+
+# app config
+app = Flask("__name__")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# config
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret_xxx'
+)
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+# silly user model
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+# create some users with ids 1 to 20       
+user = User(0)
+
+# upload file config page
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# upload page
+@app.route('/uploads', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        text = request.form['message']
+
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            import_number.importPhone(filepath=path, msg=text)
+            return redirect(url_for('upload_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=text name=message>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+# some protected url
+@app.route('/')
+@login_required
+def home():
+    return Response("Welcome")
+
+# show phones number
+@app.route('/phones')
+@login_required
+def show():
+    return Response("<h1>this is phones</h1>")
+
+# login page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']        
+        if password == config.PASSWORD and username == config.USERNAME :
+            login_user(user)
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
+
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+    
+    
+# callback to reload the user object        
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+    
+
+if __name__ == "__main__":
+    app.run()
